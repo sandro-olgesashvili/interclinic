fdb0dd733e179ebc85399345d7d0992b7a36b24a
sandro-olgesashvili <>
2023-02-27 20:37:28 +0400

    interclinic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { LaboratoryComponent } from './laboratory/laboratory.component';
+import { NewsComponent } from './news/news.component';
+import { PatientComponent } from './patient/patient.component';
+import { PartnersComponent } from './partners/partners.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroSectionComponent,
+    LaboratoryComponent,
+    NewsComponent,
+    PatientComponent,
+    PartnersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
